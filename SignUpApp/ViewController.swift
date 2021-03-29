968463e75017bbfd526e4c694c56851dfc453438
Maksim <potapovmv94@gmail.com>
2021-03-29 22:38:45 +0300

    Initial Commit

---
+++ SignUpApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUpApp
+//
+//  Created by Admin1 on 29.03.2021.
+//  Copyright © 2021 potapov_potapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
